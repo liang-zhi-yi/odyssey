9b18bd01493e47fb25b1bbe56b8eb1615a6eb79b
liang-zhi-yi <liang-zhi-yi@example.com>
2026-06-03 17:54:54 +0800

    chore: remove .next build artifacts from git tracking

--- frontend/.next/static/chunks/app/submission/[id]/page-8ee0c16d2a3e23ef.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[738,409,295,776,702,421,347,751],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
